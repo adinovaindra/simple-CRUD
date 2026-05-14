3b0da3d4d54c621f291c5a198074e1503959be59
Adinova Indra Permana <adinovapramono@yahoo.co.id>
2026-05-14 19:07:02 +0700

    Delete coba.js

--- coba.js
+++
@@ -1,9 +0,0 @@
-const jam = 'limaribu'
-const jamTerbang = Number(jam)
-console.log(jamTerbang)
-
-if (jamTerbang <= 0) {
-    console.log(`Ga sesuai sama format / jam terbang kurang nih`)
-} else {
-    console.log(`Ini baru pilot gue!`)
-}
